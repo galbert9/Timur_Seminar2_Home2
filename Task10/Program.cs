80363b395c829f8232b70db72d495f6b73a290ad
Gennady <galbert9@gmail.com>
2022-11-10 12:59:32 +0300

    Random & вторая цифра

---
+++ Task10/Program.cs
@@ -0,0 +1,6 @@
+﻿// 3-x значное число на входе, на выходе дать вторую цифру
+int a = new Random().Next(100,1000);
+Console.WriteLine($"Случайное число {a}");
+int b = a/10;
+int c = b % 10;
+Console.WriteLine($"{c}");
