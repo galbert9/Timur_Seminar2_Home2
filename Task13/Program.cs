9bad668a341f04d43891da4021405c7f4ad0624e
Gennady <galbert9@gmail.com>
2022-11-11 11:41:43 +0300

    Найти 2-ю цифру трехзначного числа

---
+++ Task13/Program.cs
@@ -0,0 +1,18 @@
+﻿// Найти 3-ю цифру заданного числа или сообщить отсуствие
+Console.WriteLine("Ввести число  не более 1,000,000");
+int b = Convert.ToInt32 (Console.ReadLine());
+
+double d = Math.Log10(b);
+
+if (d < 2) Console.WriteLine("Третьей цифры нет");
+else if (d < 3) Console.WriteLine("Третья цифра = " +b%10);
+else if (d < 4) Console.WriteLine("Третья цифра = " +b%100/10);
+else if (d < 5) Console.WriteLine("Третья цифра = " +b%1000/100);
+else if (d < 6) Console.WriteLine("Третья цифра = " +b%10000/1000);
+
+
+
+
+
+
+
---
+++ Task15/Program.cs
@@ -0,0 +1,8 @@
+﻿// Задать случайный номер дня недели и определить, является ли он выходным
+
+int a = new Random().Next(1,7);
+
+Console.WriteLine($"Случайный номер недели " +a);
+
+if (a > 5) Console.WriteLine("Выходной день");  
+else Console.WriteLine("Рабочий день");
